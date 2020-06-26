b526454847515112f9e58997f1a525b53e2d837a
rohini0709 <rohinidas.911@gmail.com>
2020-06-26 21:33:05 +0530

    Update agent.py
    Fix mistakes.

--- agent.py
+++ agent.py
@@ -22,12 +22,12 @@ class RandomAgent(Agent):
     def play(self, game):
         actions, rewards = [], []
 
-        for turn in range(self.turns):
+        for _ in range(self.turns):
             action = np.random.choice(game.get_action_space())
             reward = game.step(action)
 
             actions.append(action)
-            rewards.append(action)
+            rewards.append(reward)
 
         return actions, rewards
 
@@ -39,14 +39,14 @@ class GreedyAgent(Agent):
     def __init__(self, arms=10, turns=1000):
         super().__init__(arms, turns)
 
-        self.action_value = np.random.normal(0, 0.01, arms)
+        self.action_value = np.zeros(arms)
 
     def play(self, game):
         actions, rewards = [], []
 
         action_tracker = defaultdict(int)
 
-        for turn in range(self.turns):
+        for _ in range(self.turns):
             action = np.argmax(self.action_value)
             reward = game.step(action)
 
@@ -56,12 +56,12 @@ class GreedyAgent(Agent):
                 (reward - self.action_value[action]) / action_tracker[action]
 
             actions.append(action)
-            rewards.append(action)
+            rewards.append(reward)
 
         return actions, rewards
 
     def get_action_values(self):
-        return self.action_values
+        return self.action_value
 
     def __repr__(self):
         return "Greedy Agent"
@@ -71,7 +71,7 @@ class EpsilonGreedyAgent(Agent):
     def __init__(self, epsilon=0.1, arms=10, turns=1000):
         super().__init__(arms, turns)
 
-        self.action_value = np.random.normal(0, 0.01, arms)
+        self.action_value = np.zeros(arms)
         self.epsilon = epsilon
 
     def play(self, game):
@@ -79,7 +79,7 @@ class EpsilonGreedyAgent(Agent):
 
         action_tracker = defaultdict(int)
 
-        for turn in range(self.turns):
+        for _ in range(self.turns):
             if random.random() < self.epsilon:
                 # Take random action.
                 action = np.random.choice(game.get_action_space())
@@ -95,12 +95,12 @@ class EpsilonGreedyAgent(Agent):
                 (reward - self.action_value[action]) / action_tracker[action]
 
             actions.append(action)
-            rewards.append(action)
+            rewards.append(reward)
 
         return actions, rewards
 
     def get_action_values(self):
-        return self.action_values
+        return self.action_value
 
     def __repr__(self):
         return f"Epsilon-Greedy Agent ({self.epsilon})"
